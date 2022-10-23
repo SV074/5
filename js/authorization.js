8c2d3b5140cfc9841fc4a4f1f48ed4dfa153df10
SV074 <gabidulin174@gmail.com>
2022-10-23 19:10:29 +0300

    Выход из личного кабинета

--- js/authorization.js
+++ js/authorization.js
@@ -2,11 +2,12 @@ let loginInput = document.querySelector('.login');
 let passwordInput = document.querySelector('.password');
 let btnSend = document.querySelector('.btn');
 let info = document.getElementById('info');
+let btnExitAccount = document.querySelector('.btn-exit');
 
 let login;
 let password;
 
-
+if(localStorage != 0)
 
 
 
@@ -18,10 +19,11 @@ function sendForm() {
         login, password
     };
 
-    if(login === 'dick', password === '123') {
+    if(login === 'dick' && password === '123') {
 
         info.innerHTML = "Correct";
         localStorage.setItem('user1', JSON.stringify(user));
+        btnExitAccount.innerHTML = "X";
 
     }  else if(login === '', password === '') { 
         info.innerHTML = "Empty";
@@ -32,8 +34,18 @@ function sendForm() {
 }
 
 btnSend.addEventListener('click', () => {
-    
-    console.log(sendForm);
     sendForm();
 
 });
+
+function clearForm() {
+    loginInput.value = "";
+    passwordInput.value = "";
+    info.innerHTML = "Welcome";
+}
+
+btnExitAccount.addEventListener('click', () => {
+    localStorage.clear();
+    clearForm();
+    btnExitAccount.innerHTML = "";
+})
